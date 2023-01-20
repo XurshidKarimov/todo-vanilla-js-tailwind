1d370148deaf43b8f7d241a4f07ce063ee4b7dc9
Xurshid <kaxudi34801@gmail.com>
2023-01-20 23:09:27 +0500

    some bugs fixed. added post date&time

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let title = document.getElementById("postTitle"),
     remove = document.getElementById("deletePost"),
     change = document.getElementById("changePost"),
     postList = document.getElementById("postList"),
-    postQueue = 1,
+    postQueue = 0,
     postsArr = [];
 
 add.addEventListener("click", () => {
@@ -12,17 +12,42 @@ add.addEventListener("click", () => {
         let child = document.createElement("li"),
             childTitle = document.createElement("h3"),
             childText = document.createElement("p"),
-            postId = document.createElement("span");
+            postId = document.createElement("span"),
+            postedTime = document.createElement("time");
+
 
         child.appendChild(postId);
         child.appendChild(childTitle);
         child.appendChild(childText);
-        
+        child.appendChild(postedTime);
+
         childText.style.overflowWrap = "anywhere";
+
+        ++postQueue;
 
         postId.textContent = `ID: ${postQueue}`;
         childTitle.textContent = title.value;
         childText.textContent = postText.value;
+
+        let currentTime = new Date();
+        let hour = currentTime.getHours(),
+            minute = currentTime.getMinutes(),
+            secund = currentTime.getSeconds(),
+            day = currentTime.getDate(),
+            month = currentTime.getMonth(),
+            year = currentTime.getFullYear();
+        
+        ++month;
+
+        day = day < 10 ? `0${day}` : `${day}`;
+        month = month < 10 ? `0${month}` : `${month}`;
+        hour = hour < 10 ? `0${hour}` : `${hour}`;
+        minute = minute < 10 ? `0${minute}` : `${minute}`;
+        secund = secund < 10 ? `0${secund}` : `${secund}`;
+
+        postedTime.textContent = `Posted on: ${day}.${month}.${year} / ${hour}:${minute}:${secund}`;
+        postedTime.setAttribute("date", currentTime);
+        postedTime.style.textAlign = "end";
 
         child.classList.add("post-list-item");
 
@@ -33,7 +58,6 @@ add.addEventListener("click", () => {
         title.value = '';
         postText.value = '';
 
-        ++postQueue;
     }
     else{
         if(!title.value && !postText.value){
@@ -55,26 +79,42 @@ remove.addEventListener("click", () => {
     let idInput = document.getElementById("idInput");
     let checkIdRemoveBtn = remove.getAttribute("id");
     
-    
     if(checkIdRemoveBtn == "deletePost"){
-        idInput.style.display = "inline";
-        idInput.focus();
-        remove.setAttribute("id", "deletePostActive");    
+        if(postQueue > 0){
+            idInput.style.display = "inline";
+            idInput.focus();
+            remove.setAttribute("id", "deletePostActive"); 
+        }
+        else{
+            alert("No items for remove.");
+            title.focus();            
+        }
     }
     else{
-        if(idInput.checkValidity() && +idInput.value <= postQueue){
+        if(!idInput.value){
+            idInput.style.display = "none";
+            remove.setAttribute("id", "deletePost");
+        }
+        else if(idInput.checkValidity() && +idInput.value <= postQueue){
             if(postList.hasChildNodes()){
                 postList.removeChild(postList.children[(+idInput.value)-1]);
                 idInput.value = '';
                 idInput.style.display = "none";
                 remove.setAttribute("id", "deletePost");
+                --postQueue;
+            }
+        }
+        else{
+            if(!idInput.checkValidity()){
+                alert("Entered data not valid. Enter only number.");
+                idInput.value = '';
+                idInput.focus();
             }
             else{
-                alert("No items for delete");
-                idInput.style.display = "none";
-                remove.setAttribute("id", "deletePost");
+                alert(`Data with ID: ${idInput.value} not found. Try again.`);
+                idInput.value = '';
+                idInput.focus();
             }
-           
         }
     }
 })
