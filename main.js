a95f63ccf2d59b697abdbbbbc22a5e688efcd9ee
Xurshid <kaxudi34801@gmail.com>
2023-01-22 05:04:02 +0500

    buttons delete and change temporarly commented in html. Maybe next time delete. Button CHANGE removes to component-post. Restyling

--- main.js
+++ main.js
@@ -11,20 +11,20 @@ let author = document.getElementById("postAuthor"),
     postsArr = [];
 
 //elements must be check for empty
-let postElements = [author, title, postText];
+let postElements = [author, title];
 
 
 // ADD-BTN
 
 add.addEventListener("click", () => {
-
+    postText.innerText = postText.innerText.replace(/^\s+|\s+$/gm, '');
     //checking values to empty or not
     postElements.forEach(element => {
         element.value = element.value.replace(/^\s+|\s+$/gm, '');
     })
 
     // check elements for all values are not empty
-    if(title.value && postText.value && author.value){
+    if(title.value && postText.innerText && author.value){
         // creating elements for child tag <li>
         let child = document.createElement("li"),
             childAuthor = document.createElement("h3")
@@ -48,10 +48,18 @@ add.addEventListener("click", () => {
         ++postQueue;
         
         //join data in child-elements
-        postId.textContent = `ID: ${postQueue}`;
-        childAuthor.textContent = `Author: ${author.value}`;
+        // postId.textContent = `ID: ${postQueue}`;
+        
+        let authorNote = document.createElement("span");
+        childAuthor.appendChild(authorNote);
+        authorNote = author.value;
+        // authorNote.classList.add("font-bold");
+
+        // let authorClassTailwind = ["font-bold"];
+
+        childAuthor.textContent = `Author: ${authorNote}`;
         childTitle.textContent = title.value;
-        childText.textContent = postText.value;
+        childText.textContent = postText.innerText;
 
         //time&date
         let currentTime = new Date();
@@ -90,7 +98,7 @@ add.addEventListener("click", () => {
         postList.appendChild(child);
         
         //AFTER ADDING INPUTS CLEARED and focused to first input
-        author.value = title.value = postText.value = '';
+        author.value = title.value = postText.innerText = '';
         author.focus();
 
     }
@@ -114,46 +122,93 @@ add.addEventListener("click", () => {
 
 // REMOVE-BTN
 
-remove.addEventListener("click", () => {
-    let idInput = document.getElementById("idInput");
-    let checkIdRemoveBtn = remove.getAttribute("id");
+// remove.addEventListener("click", () => {
+//     let idInput = document.getElementById("idInput");
+//     let checkIdRemoveBtn = remove.getAttribute("id");
     
-    if(checkIdRemoveBtn == "deletePost"){
-        if(postQueue > 0){
-            idInput.style.display = "inline";
-            idInput.focus();
-            remove.setAttribute("id", "deletePostActive"); 
-        }
-        else{
-            alert("No items for remove.");
-            author.focus();            
-        }
-    }
-    else{
-        if(!idInput.value){
-            idInput.style.display = "none";
-            remove.setAttribute("id", "deletePost");
-        }
-        else if(idInput.checkValidity() && +idInput.value <= postQueue){
-            if(postList.hasChildNodes()){
-                postList.removeChild(postList.children[(+idInput.value)-1]);
-                idInput.value = '';
-                idInput.style.display = "none";
-                remove.setAttribute("id", "deletePost");
-                --postQueue;
-            }
-        }
-        else{
-            if(!idInput.checkValidity()){
-                alert("Entered data not valid. Enter only number.");
-                idInput.value = '';
-                idInput.focus();
-            }
-            else{
-                alert(`Data with ID: ${idInput.value} not found. Try again.`);
-                idInput.value = '';
-                idInput.focus();
-            }
-        }
-    }
-})
+//     if(checkIdRemoveBtn == "deletePost"){
+//         if(postQueue > 0){
+//             idInput.style.display = "inline";
+//             idInput.focus();
+//             remove.setAttribute("id", "deletePostActive"); 
+//         }
+//         else{
+//             alert("No items for remove.");
+//             author.focus();            
+//         }
+//     }
+//     else{
+//         if(!idInput.value){
+//             idInput.style.display = "none";
+//             remove.setAttribute("id", "deletePost");
+//         }
+//         else if(idInput.checkValidity() && +idInput.value <= postQueue){
+//             if(postList.hasChildNodes()){
+//                 postList.removeChild(postList.children[(+idInput.value)-1]);
+//                 idInput.value = '';
+//                 idInput.style.display = "none";
+//                 remove.setAttribute("id", "deletePost");
+//                 --postQueue;
+//                 author.focus();
+//             }
+//         }
+//         else{
+//             if(!idInput.checkValidity()){
+//                 alert("Entered data not valid. Enter only number.");
+//                 idInput.value = '';
+//                 idInput.focus();
+//             }
+//             else{
+//                 alert(`Data with ID: ${idInput.value} not found. Try again.`);
+//                 idInput.value = '';
+//                 idInput.focus();
+//             }
+//         }
+//     }
+// })
+
+//CHANGE-BTN
+
+// change.addEventListener("click", () => {
+//     let idChange = document.getElementById("idChange");
+//     let checkIdChangeBtn = change.getAttribute("id");
+    
+//     if(checkIdChangeBtn == "changePost"){
+//         if(postQueue > 0){
+//             idChange.style.display = "inline";
+//             idChange.focus();
+//             change.setAttribute("id", "changePostActive"); 
+//         }
+//         else{
+//             alert("No items for change.");
+//             author.focus();            
+//         }
+//     }
+//     else{
+//         if(!idChange.value){
+//             idChange.style.display = "none";
+//             change.setAttribute("id", "changePost");
+//         }
+//         else if(idChange.checkValidity() && +idChange.value <= postQueue){
+//             if(postList.hasChildNodes()){
+//                 let postListChild = postList.children[(+idChange.value)-1];
+//                 idInput.value = '';
+//                 idInput.style.display = "none";
+//                 remove.setAttribute("id", "deletePost");
+//                 --postQueue;
+//             }
+//         }
+//         else{
+//             if(!idInput.checkValidity()){
+//                 alert("Entered data not valid. Enter only number.");
+//                 idInput.value = '';
+//                 idInput.focus();
+//             }
+//             else{
+//                 alert(`Data with ID: ${idInput.value} not found. Try again.`);
+//                 idInput.value = '';
+//                 idInput.focus();
+//             }
+//         }
+//     }  
+// })
